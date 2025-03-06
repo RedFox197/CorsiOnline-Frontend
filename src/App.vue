64e450134b60f020e7919a25fdc26c61c64243b5
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-06 15:46:54 +0100

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/corso">Corso</RouterLink>
@@ -24,65 +19,51 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
+  position: fixed;
+
+  top: 0;
+
+  left: 0;
+
   width: 100%;
-  font-size: 12px;
+
+  background-color: #42b983;
+
+  padding: 1rem 0;
+
   text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  z-index: 1000;
 }
 
 nav a {
   display: inline-block;
+
   padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+
+  color: white;
+
+  text-decoration: none;
+
+  font-weight: bold;
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a.router-link-exact-active {
+  text-decoration: underline;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+header {
+  margin-top: 60px; /* Spazio per la barra di navigazione */
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  text-align: center;
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  padding: 2rem 0;
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+.logo {
+  display: block;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  margin: 0 auto 2rem;
 }
 </style>
