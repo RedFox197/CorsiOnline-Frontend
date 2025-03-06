663c96f4e4a8628d3d16c97f7d852dc5e8fcb918
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-06 21:01:31 +0100

    clean up e refactoring

--- src/App.vue
+++ src/App.vue
@@ -20,31 +20,20 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style scoped>
 nav {
-
   top: 0;
-
   left: 0;
-
   width: 100%;
-
   background-color: #42b983;
-
   padding: 1rem 0;
-
   text-align: center;
-
   z-index: 1000;
 }
 
 nav a {
   display: inline-block;
-
   padding: 0 1rem;
-
   color: white;
-
   text-decoration: none;
-
   font-weight: bold;
 }
 
@@ -53,13 +42,11 @@ nav a.router-link-exact-active {
 }
 
 header {
-
   text-align: center;
 }
 
 .logo {
   display: block;
-
   margin: 0 auto 2rem;
 }
 </style>
