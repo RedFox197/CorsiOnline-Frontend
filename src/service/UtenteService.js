c6d0486cd61f15024a7d89b1199b0393e6b57df0
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2025-03-11 15:31:11 +0100

    utenteservice

---
+++ src/service/UtenteService.js
@@ -0,0 +1,99 @@
+// src/services/api.js
+
+/**
+ * @typedef {Object} Corso
+ * @property {number} id
+ * @property {string} titolo
+ * @property {string} descrizione
+ *
+ *
+ * @typedef {Object} Classe
+ * @property {number} id
+ * @property {string} nome
+ * @property {string} dataInizio
+ * @property {string} dataFine
+ * @property {Corso} corso
+ * @property {Utente} docente
+ *
+ * @typedef {Object} Utente
+ * @property {number} id
+ * @property {string} nome
+ * @property {string} cognome
+ * @property {string} email
+ * @property {string} ruolo
+ * */
+
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8080/api/v1/utenti', // Aggiungi l'URL base della tua API
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+/**
+ * Recupera tutti gli utenti
+ * @returns {Promise<Array<Utente>>}
+ */
+async function findAll() {
+  const response = await apiClient.get();
+  return response.data;
+}
+
+/**
+ * Recupera tutti gli utenti
+ * @param id {number}
+ * @returns {Promise<Utente>}
+ */
+async function findById(id) {
+  const response = await apiClient.get("/"+id);
+  return response.data;
+}
+
+/**
+ * Recupera tutti gli utenti
+ * @param id {number}
+ * @returns {Promise<Array<Classe>>}
+ */
+async function getClassi(id) {
+  const response = await apiClient.get("/"+id+"/classi");
+  return response.data;
+}
+
+/**
+ * Recupera tutti gli utenti
+ * @param id {number}
+ * @returns {Promise<Array<Classe>>}
+ */
+async function getEsami(id) {
+  const response = await apiClient.get("/"+id+"/esami");
+  return response.data;
+}
+
+/**
+ * @param {Utente} utente
+ */
+async function save(utente) {
+  return await apiClient.post("", utente);
+}
+
+//update con un path /utenti/id put poi simile al save anche con jsdoc
+/**
+ * @param {number} id
+ * @param {Utente} utente
+ */
+async function update(id, utente) {
+  return await apiClient.put("/"+id, utente);
+}
+
+/**
+ * @param {number} id
+ */
+async function deleteu(id) {
+  return await apiClient.delete("/"+id);
+}
+
+export default {
+  findAll, findById, getClassi, getEsami, save, update, deleteu
+}
